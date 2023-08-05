7c97e478b080d7487bf240b40b489e649f103b04
Rqwannn <mohwannn1@gmail.com>
2023-08-05 23:41:52 +0700

    build home page

--- android/lib/views/home.dart
+++ android/lib/views/home.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:forum_app/controllers/post_controller.dart';
+import 'package:forum_app/views/widgets/post_data.dart';
+import 'package:forum_app/views/widgets/post_field.dart';
+import 'package:get/get.dart';
 
 class Home extends StatefulWidget {
   const Home({super.key});
@@ -10,7 +14,7 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-  // final PostController _postController = Get.put(PostController());
+  final PostController _postController = Get.put(PostController());
   final TextEditingController _textController = TextEditingController();
 
   @override
@@ -23,7 +27,75 @@ class _HomeState extends State<Home> {
         centerTitle: true,
       ),
       body: SingleChildScrollView(
-        
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: RefreshIndicator(
+            onRefresh: () async {
+              await _postController.getAllPosts();
+            },
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                PostFIeld(
+                  hintText: 'What do you want to ask?',
+                  controller: _textController,
+                ),
+                // const SizedBox(
+                //   height: ,
+                // ),
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.black,
+                    elevation: 0,
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 50,
+                      vertical: 10,
+                    ),
+                  ),
+                  onPressed: () async {
+                    await _postController.createPost(
+                      content: _textController.text.trim(),
+                    );
+                    _textController.clear();
+                    _postController.getAllPosts();
+                  },
+                  child: Obx(() {
+                    return _postController.isLoading.value
+                        ? const CircularProgressIndicator()
+                        : const Text('Post');
+                  }),
+                ),
+                const SizedBox(
+                  height: 30,
+                ),
+                const Text('Posts'),
+                const SizedBox(
+                  height: 20,
+                ),
+                Obx(() {
+                  return _postController.isLoading.value
+                      ? const Center(
+                          child: CircularProgressIndicator(),
+                        )
+                      : ListView.builder(
+                          shrinkWrap: true,
+                          physics: const NeverScrollableScrollPhysics(),
+                          itemCount: _postController.posts.value.length,
+                          itemBuilder: (context, index) {
+                            return Padding(
+                              padding:
+                                  const EdgeInsets.symmetric(vertical: 8.0),
+                              child: PostData(
+                                post: _postController.posts.value[index],
+                              ),
+                            );
+                          },
+                        );
+                }),
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
